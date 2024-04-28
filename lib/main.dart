8792f51f8961b6b8bc580fd8fe2acb5cc7b5c411
prabinrag234 <94599046+prabinrag234@users.noreply.github.com>
2024-04-28 14:37:33 +0530

    Lib and Linux
    0.5

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//Application
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => LocaleProvider(),
+        builder: (context, child) {
+          final provider = Provider.of<LocaleProvider>(context);
+          return MaterialApp(
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: provider.locale,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const MyHomePage(),
+          );
+        },
+      );
+}
+
+//Welcome Screen
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Localization App'),
+        actions: const [
+          LocaleSwitcherWidget(),
+          SizedBox(width: 12),
+        ],
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(AppLocalizations.of(context)!.welcome),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// locale_provider
+class LocaleProvider extends ChangeNotifier {
+  Locale _locale = const Locale("en");
+  Locale get locale => _locale;
+  void setLocale(Locale locale) {
+    if (!AppLocalizations.supportedLocales.contains(locale)) return;
+    _locale = locale;
+    notifyListeners();
+  }
+}
+
+//locale_switcher_widget
+class LocaleSwitcherWidget extends StatelessWidget {
+  const LocaleSwitcherWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<LocaleProvider>(context);
+    final locale = provider.locale;
+    return DropdownButtonHideUnderline(
+      child: DropdownButton(
+        value: locale,
+        icon: Container(width: 12),
+        items: AppLocalizations.supportedLocales.map(
+          (nextLocale) {
+            return DropdownMenuItem(
+              value: nextLocale,
+              onTap: () {
+                final provider =
+                    Provider.of<LocaleProvider>(context, listen: false);
+                provider.setLocale(nextLocale);
+              },
+              child: Center(
+                child: Text(nextLocale.toString()),
+              ),
+            );
+          },
+        ).toList(),
+        onChanged: (_) {},
+      ),
+    );
+  }
+}
